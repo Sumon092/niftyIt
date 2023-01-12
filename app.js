71e1321d4aa9772c0611e3f4645d5a59d0a3f5bd
Sumon <m.sumon@outlook.com>
2023-01-12 16:01:02 +0600

    authentication implimented with jwt

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 //routes
 const userRoute = require("./routes/user.route");
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { signup, login } = require('../controllers/user.controller');
+const verifyJwt = require('../middlewares/verifyJwt');
 
 
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login", verifyJwt, login);
 
 module.exports = router;
