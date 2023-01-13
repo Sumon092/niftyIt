6be20bd48e4a620edd069d6ac3a7d31131df1e3e
Sumon <m.sumon@outlook.com>
2023-01-13 09:45:05 +0600

    all user and finduser by id done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -58,3 +58,25 @@ exports.updateUser = async (req, res, next) => {
         }).catch(error => next(error))
 };
 
+
+
+// get user
+exports.getUserById = async (req, res, next) => {
+
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user);
+    } catch (error) {
+        next(error)
+    }
+};
+exports.getUser = async (req, res, next) => {
+
+    try {
+        const user = await User.find(req.body)
+        res.status(200).json(user);
+    } catch (error) {
+        next(error)
+    }
+};
+
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { signup, login, updateUser } = require('../controllers/user.controller');
+const { signup, login, updateUser, getUser, getUserById } = require('../controllers/user.controller');
 const verifyJwt = require('../middlewares/verifyJwt');
-const User = require('../models/User');
+
 
 
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", verifyJwt, login);
+router.post("/login", login);
 router.put("/updateUser/:id", updateUser);
+router.get("/getuser/:id", getUserById);
+router.get("/getuser", getUser);
 
 module.exports = router;
